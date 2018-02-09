fec3aa802185f25bb59262c84004adbb0bfb147a
Dana Manoli <dmanoli1@gmail.com>
2018-02-09 11:33:30 -0500

    more initializing --added node modes

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,69 @@
-const gulp = require("gulp");
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const babel = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
 const reload = browserSync.reload;
-//a task to compile our sass
-// gulp.task('bacon',function(){
-// 	console.log('bbbbb');
-// });
+const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+
+
 gulp.task('styles', () => {
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('./public/styles'))
-		.pipe(reload({stream: true}));
 });
 
-//task that complie js
-gulp.task('scripts', () => {
-	return gulp.src('./dev/scripts/main.js')
-		.pipe(babel({
+gulp.task('js', () => {
+	return browserify('dev/scripts/**/*.js', {debug: true})
+		.transform('babelify', {
+			sourceMaps: true,
 			presets: ['es2015']
+		})
+		.bundle()
+		.on('error',notify.onError({
+			message: "Error: <%= error.message %>",
+			title: 'Error in JS 💀'
 		}))
-		.pipe(gulp.dest('./public/scripts'))
-		.pipe(reload({stream: true}));
+		// .pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('public/scripts'))
+		.pipe(reload({stream:true}));
 });
 
-//a task to watch all of my other tasks
-gulp.task('watch', function() {
-	gulp.watch('./dev/scripts/*.js', ['scripts']);
-	gulp.watch('./dev/styles/*.scss', ['styles']);
-	gulp.watch('*.html', reload);
+gulp.task('assets', () => {
+	return gulp.src('./dev/assets/**/*')
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.jpegtran({ progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(gulp.dest('./public/assets/'))
+		.pipe(reload({ stream: true }));
 });
 
-gulp.task('browser-sync', () => {
-  browserSync.init({
-    server: '.'
-  })
+gulp.task('bs', () => {
+	return browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
 });
 
-gulp.task('default', ['browser-sync','styles', 'scripts', 'watch']);
+gulp.task('default', ['bs', 'styles', 'assets'], () => {
+	gulp.watch('dev/**/*.js', ['scripts']);
+	gulp.watch('dev/**/*.scss', ['styles']);
+	gulp.watch('./dev/assets/**/*', ['assets']);
+	gulp.watch('./public/styles/style.css', reload);
+});
